d2c3e3319c62b5c2e9e4cdb173cb6e1f7f1eb2f3
Clmt2707 <clement.deboeuf@gmail.com>
2024-04-03 21:21:15 +0200

    ajout des projets sur Portfolio

--- src/components/Projet/Projet.jsx
+++ src/components/Projet/Projet.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Project = ({ projet }) => {
     return(
         <div className="project">
-            <img src={projet.image} alt={projet.title} />
+            <img src={require(`../../assets/images/${projet.image}`)} alt={projet.title}/>
             <h3>{projet.title}</h3>
         </div>
     );
