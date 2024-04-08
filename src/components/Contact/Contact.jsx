cc886cd40f40d5d3ce15f7a582311678892ba6ee
Clmt2707 <clement.deboeuf@gmail.com>
2024-04-08 13:00:53 +0200

    Ajout section contact (sans les couleurs)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,14 @@ import '../Contact/Contact.scss';
 const Contact = () => {
     return (
         <section id="contact">
-            <div className="contact"></div>
+            <h2>Contact</h2>
+            <p>N'hésitez pas à me contacter</p>
+            <form>
+                <input type="text" placeholder="Nom" />
+                <input type="email" placeholder="Adresse e-mail"/>
+                <textarea placeholder="Votre message"/>
+                <button type="submit">Me contacter</button>
+            </form>
         </section>
     );
 };
