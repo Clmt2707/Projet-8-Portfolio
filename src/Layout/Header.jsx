aeb3f48e5cb107a14a5816c811bf33d88e408819
Clmt2707 <clement.deboeuf@gmail.com>
2024-04-10 20:37:10 +0200

    ajout menu hamburger

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -9,19 +9,14 @@ import { useState } from "react";
 function Header() {
     //Gestion de l'état du menu hamburger
     const [isOpen, setIsOpen] = useState(false);
+   
     //fonction de bascule de l"état
     const toggleMenu = () => {
       setIsOpen(!isOpen);
-      const nav = document.querySelector('.header__nav');
-      if(!isOpen) {
-        nav.classList.add('active');
-      } else {
-        nav.classList.remove('active');
-      }
     };
 
     //Réglages d'offset pour les liens en fonciton de la taille d'écran
-    const offset = window.innerWidth < 768 ? -70 : -100;
+    const offset = window.innerWidth < 768 ? -90 : -100;
 
     //Taille de logo en fonction de l'écran
     const image = window.innerWidth <768 ? SmallLogo : Logo;
@@ -34,22 +29,21 @@ function Header() {
             </a>
         </div>
         <nav className={`header__nav ${isOpen ? 'active' : ''}`}>
-          <div className="header__menu-toggle" onClick={toggleMenu}>
-            <div className={`header__menu-bar ${isOpen ? 'open' : ''}`}></div>
-            <div className={`header__menu-bar ${isOpen ? 'open' : ''}`}></div>
-            <div className={`header__menu-bar ${isOpen ? 'open' : ''}`}></div>
-          </div>
           <ul>
-            <li><Link activeClass="active" to="home" spy smooth offset={offset} duration={500}>Home</Link></li>
-            <li><Link activeClass="active" to="about" spy smooth offset={offset} duration={500}>About</Link></li>
-            <li><Link activeClass="active" to="portfolio" spy smooth offset={offset} duration={500}>Portfolio</Link></li>
-            <li><Link activeClass="active" to="skills" spy smooth offset={offset} duration={500}>Skills</Link></li>
-            <li><Link activeClass="active" to="contact" spy smooth offset={offset} duration={500}>Contact</Link></li>
+            <li><Link activeClass="active" to="home" spy smooth offset={offset} duration={500} onClick={() => setIsOpen(false)}>Home</Link></li>
+            <li><Link activeClass="active" to="about" spy smooth offset={offset} duration={500} onClick={() => setIsOpen(false)}>About</Link></li>
+            <li><Link activeClass="active" to="portfolio" spy smooth offset={offset} duration={500} onClick={() => setIsOpen(false)}>Portfolio</Link></li>
+            <li><Link activeClass="active" to="skills" spy smooth offset={offset} duration={500} onClick={() => setIsOpen(false)}>Skills</Link></li>
+            <li><Link activeClass="active" to="contact" spy smooth offset={offset} duration={500} onClick={() => setIsOpen(false)}>Contact</Link></li>
           </ul>
         </nav>
+        <div className={`header__menu-toggle ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+            <div className="header__menu-bar"></div>
+            <div className="header__menu-bar"></div>
+            <div className="header__menu-bar"></div>
+          </div>
       </header>
     );
   }
- 
 
 export default Header;
