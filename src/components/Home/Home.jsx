5a3828e0d1ca0162221648319bd554333612c68b
Clmt2707 <clement.deboeuf@gmail.com>
2024-04-10 21:24:14 +0200

    ajout phot page home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "../Home/Home.scss";
 import SocialLinks from "../SocialLinks/SocialLinks";
-/*import photo from '../../assets/clement_deboeuf.webp';*/
+import photo from '../../assets/clement_sticker.webp';
 
 const Home = () => {
     return (
         <section id="home">
             <div className="homeSection">
-                <h1>Hello,<br /> I'm Clement Deboeuf.<br />
+                <h1>Hello,<br />I'm Clement Deboeuf.<br />
                 <span>Web Developper</span></h1>
+                <img src={photo} alt="Sticker représentant Clement Deboeuf" />
                 <SocialLinks />
             </div>
         </section>
